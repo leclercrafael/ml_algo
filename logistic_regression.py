13284a7aa15395f77bf95faf84f9a1224ac27c8a
Rafaël Leclerc <rafael.leclerc.rl@gmail.com>
2025-12-07 13:35:30 +0100

    firt

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,3 +4,21 @@ import matplotlib.pylab as plt
 
 from linear_regression import myLinearRegression
 
+class myLogisticRegression:
+
+    def __init__(self, learning_rate : float = 0.01, n_iter : int = 10000):
+        self.learning_rate = learning_rate
+        self.n_iter = n_iter
+
+    def sigmoid(z : float) -> float:
+        return 1/(1+np.exp(-z))
+    
+    def loss_function(y : np.array, y_pred : np.array) -> float:
+        return -np.mean(y*np.log(y_pred) + (1-y)*np.log(1-y_pred))
+    
+    def dloss_db(self, x : np.array, y : np.array, w : np.array, b : float) -> float : 
+        N = len(x)
+        y_pred = self.prediction(x, w, b)
+        return 2*np.sum(y_pred - y)/N
+    
+    
