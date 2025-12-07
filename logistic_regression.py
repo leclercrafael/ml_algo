17073e9cdf83c5c8553fca6ebf9fd1f17bc6d497
Rafaël Leclerc <rafael.leclerc.rl@gmail.com>
2025-12-07 13:38:09 +0100

    architecture

--- logistic_regression.py
+++ logistic_regression.py
@@ -10,6 +10,9 @@ class myLogisticRegression:
         self.learning_rate = learning_rate
         self.n_iter = n_iter
 
+    def linear(self, x : np.array, w : np.array, b : float) -> np.array :
+        return (np.dot(x,w) + b)
+
     def sigmoid(z : float) -> float:
         return 1/(1+np.exp(-z))
     
@@ -21,4 +24,64 @@ class myLogisticRegression:
         y_pred = self.prediction(x, w, b)
         return 2*np.sum(y_pred - y)/N
     
+    def dloss_dw(self, x : np.array, y : np.array, w : np.array, b : float) -> np.array :
+        N = len(x)
+        y_pred = self.prediction(x, w, b)
+        return 2*np.dot(x.T, (y_pred-y))/N
     
+    def fit(self, X : np.array):
+
+        X = np.array(X)
+
+        self.cost_history = []
+         
+        self.x = X[:,:-1]
+        self.y = X[:,-1]
+
+        self.w = np.array([1 for i in range(len(self.x[0]))])
+        self.b = 0
+
+        for i in range (self.n_iter):
+
+            dmse_db = self.dmse_db(self.x, self.y, self.w, self.b)
+            dmse_dw = self.dmse_dw(self.x, self.y, self.w, self.b)
+
+            self.b = self.b -self.learning_rate*dmse_db
+            self.w = self.w -self.learning_rate*dmse_dw
+
+            current_cost = self.mse(self.x, self.y, self.w, self.b)
+            self.cost_history.append(current_cost)
+
+    def plot_learning_curve(self):
+        
+        plt.plot(self.cost_history)
+        plt.title("Évolution de la fonction de coût (MSE)")
+        plt.xlabel("Itérations")
+        plt.ylabel("Coût")
+        plt.show()
+
+    @property
+
+    def coef(self) -> None :
+        print (f'Weights w : {self.w}')
+        print (f'Biais b : {self.b}')
+
+    @property
+
+    def final_mse(self):
+        return self.mse(self.x, self.y, self.w, self.b)
+
+
+            
+
+        
+
+
+
+        
+
+
+        
+
+
+
